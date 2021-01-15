ea1c9c6a1b22b5a9f6a271c576f744e322936e2e
amandomoreno <amandomoreno47@gmail.com>
2021-01-15 10:11:03 -0600

    resources router

---
+++ routes/resources.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const resourcesCtrl = require('../controllers/resources');
+
+// Protected Routes
+router.use(require('../config/auth'));
+router.post('/', checkAuth, resourcesCtrl.create);
+router.get('/random', checkAuth, resourcesCtrl.randomResources)
+router.post('/search', checkAuth, resourcesCtrl.search);
+router.put('/:id', checkAuth, resourcesCtrl.deleteFromSaved);
+
+// Admin Routes
+router.put('/:id', checkAuth, resourcesCtrl.updateResource);
+router.delete('/:id', checkAuth, resourcesCtrl.deleteResource);
+
+function checkAuth(req, res, next) {
+	if (req.user) return next();
+	return res.status(401).json({msg: 'Not Authorized'});
+}
+
+module.exports = router;
