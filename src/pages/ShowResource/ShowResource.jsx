229016e55629fab8d91de0b67ab61fa5e4f1b3d9
jpaige343 <jenniferoneill129@gmail.com>
2021-01-15 08:55:27 -0600

    clean up pseduo code for ShowResource

--- src/pages/ShowResource/ShowResource.jsx
+++ src/pages/ShowResource/ShowResource.jsx
@@ -5,9 +5,9 @@ PSEUDO CODE:
     -render a div and pass resource props to be shown on page
         - create a text-box for user notes
         - Create a form to submit notes
-        - ref formRef
-        - add onSubmit to handle handleSubmit function
-        - add onChange event handler to text box
-        - Create submit button to render information on the same page
+            - ref formRef
+            - add onSubmit to handle handleSubmit function
+            - add onChange event handler to text box
+            - Create submit button to render notes on the same page
 
 */
