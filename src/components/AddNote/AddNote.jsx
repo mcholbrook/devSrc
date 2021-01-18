f3b8a26720d880040897e64af0a84b6cfe94e11c
jpaige343 <jenniferoneill129@gmail.com>
2021-01-17 21:42:55 -0600

    store notes in state

--- src/components/AddNote/AddNote.jsx
+++ src/components/AddNote/AddNote.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-  const AddNewNote = () => {
-      return ( 
-    <h1>note!</h1>
-       );
-  }
+class AddNote extends Component {
+    state = {
+        invalidForm: true,
+        formData: {
+          note: "",
+        },
+        resources: [],
+      };
+    
+      formRef = React.createRef();
+    
+      handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.handleAddNote(this.state.formData);
+      };
+    
+      handleChange = (e) => {
+        const formData = {
+          ...this.state.formData,
+          [e.target.name]: e.target.value,
+        };
+        this.setState({
+          formData,
+          invalidForm: !this.formRef.current.checkValidity(),
+        });
+      };
+
+    render() { 
+        return (
+            <>
+            <div className="comment box">
+            <form ref={this.formRef} onSubmit={this.handleSubmit}>
+            <input name="note" type="text" value={this.state.formData.note} onChange={this.handleChange}></input>
+                <button>Add Note</button>
+            </form>
+            </div>
+            </>
+        );
+    }
+}
+ 
+
    
-  export default AddNewNote;
+  export default AddNote;
