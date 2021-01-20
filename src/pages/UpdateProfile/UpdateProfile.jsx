343b96fdfae2e5a18e91bdb385278ce60cb12c9e
jpaige343 <jenniferoneill129@gmail.com>
2021-01-19 19:43:58 -0600

    add update profile form

--- src/pages/UpdateProfile/UpdateProfile.jsx
+++ src/pages/UpdateProfile/UpdateProfile.jsx
@@ -22,15 +22,12 @@ class UpdateProfile extends Component {
       github: "",
       linkedIn: "",
       website: "",
+      profileId: this.props.profile._id
     },
+    
   };
 
   formRef = React.createRef();
-
-//   async componentDidMount() {
-//     const users = await getAllUsers();
-//     this.setState({ users });
-//   }
 
 // handleSubmit = e => {
 //     e.preventDefault();
@@ -42,8 +39,6 @@ class UpdateProfile extends Component {
       ...this.state.formData,
       [e.target.name]: e.target.value,
     };
-    console.log(this.state)
-    console.log(this.props.myResources)
     this.setState({
       formData,
       invalidForm: !this.formRef.current.checkValidity(),
@@ -53,9 +48,10 @@ class UpdateProfile extends Component {
   render() {
     return (
       <>
-      
         <h1>Update Profile</h1>
         <h4>Connect with your classmates</h4>
+        {this.props.profile._id ?
+        // Update Form 
         <div id="UserProfile" className="row">
           <form
             className="col s12"
@@ -118,7 +114,72 @@ class UpdateProfile extends Component {
               Update
             </button>
           </form>
-        </div>
+          </div>
+        : // Add Info Form
+        <div id="UserProfile" className="row">
+          <form
+            className="col s12"
+            ref={this.formRef}
+            onSubmit={this.handleSubmit}
+          >
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="github"
+                  id="github"
+                  type="text"
+                  autoComplete="off"
+                  className="active"
+                  value={this.state.formData.url}
+                  onChange={this.handleChange}
+                />
+                <label className="card-text" htmlFor="github">
+                GitHub Profile (url):
+                </label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="linkedIn"
+                  id="linkedIn"
+                  type="text"
+                  autoComplete="off"
+                  className="active"
+                  value={this.state.formData.url}
+                  onChange={this.handleChange}
+                />
+                <label className="card-text" htmlFor="linkedIn">
+                LinkedIn Profile (url):
+                </label>
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12">
+                <input
+                  name="website"
+                  id="website"
+                  type="text"
+                  autoComplete="off"
+                  className="active"
+                  value={this.state.formData.url}
+                  onChange={this.handleChange}
+                />
+                <label className="card-text" htmlFor="website">
+                Website (url):
+                </label>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="btn"
+              disabled={this.state.invalidForm}
+            >
+              Update
+            </button>
+          </form>
+          </div>
+        }  
       </>
     );
   }
