e874a6e55fcce5f685c2a3cc5b57055c30666910
mcholbrook <mirandacholbrook@gmail.com>
2021-01-21 12:01:03 -0500

    Add flashcard flip

--- src/components/BackFlashCard/BackFlashCard.jsx
+++ src/components/BackFlashCard/BackFlashCard.jsx
@@ -4,10 +4,7 @@ import '../FlashCard/FlashCard.css'
 const BackFlashCard = (props) => {
   return ( 
     <div className="flash-card back" onClick={props.handleClick}>
-      {/* <div className="flash-card back"> */}
       <p>{props.flashCard.backSide}</p>
-      {/* <button onClick={props.handleClick}>Click to flip</button> */}
-      {/* </div> */}
     </div>
    );
 }
